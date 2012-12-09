0d2a74a3b960a679b5fc2f9dc71ed0ece63818ee
Dave Herren <David.Herren@yahoo.com>
2012-12-09 18:17:29 -0500

    Setup first of many new test files
    Taking a new approach to testing... each file is a module. Each module
    covers a single model that represents a particular type of scenario the
    app needs to handle. Every module should have the same number of
    tests... one for each piece of functionality of the frame work. So every
    module has complete test coverage.

---
+++ simpleTypes-qunit-tests.js
@@ -0,0 +1,41 @@
+﻿/// <reference path="FromTo-Mapping-qunit-tests.js" />
+var testData = {};
+module("Simple Types", {
+    setup: function () {
+        ko.viewmodel.logging = true;
+
+        testData.model = {
+            stringProp: "test",
+            number: 5,
+            date: new Date("01/01/2001"),
+            emptyArray: []
+        };
+
+    },
+    teardown: function () {
+        ko.viewmodel.logging = false;
+        delete testData.model;
+    }
+});
+
+
+test("Create Viewmodel", function () {
+        
+    var model, viewmodel;
+    model = testData.model,
+    viewmodel = ko.viewmodel.fromModel(model);
+
+    deepEqual(viewmodel().stringProp(), model.stringProp, "String Test");
+    deepEqual(viewmodel().number(), model.number, "Number Test");
+    deepEqual(viewmodel().date(), model.date, "Date Test");
+    deepEqual(viewmodel().emptyArray(), model.emptyArray, "Array Test");
+});
+
+test("", function () {
+
+    var model, viewmodel;
+    model = testData.model,
+    viewmodel = ko.viewmodel.fromModel(model);
+
+    deepEqual(null, null, "");
+});
