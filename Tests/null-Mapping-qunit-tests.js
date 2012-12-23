244856be952b6e23ed949f6b3557ae9574617800
Dave Herren <David.Herren@yahoo.com>
2012-12-23 15:55:08 -0500

    Testing and code fixes for handling nulls completed

--- Tests/null-Mapping-qunit-tests.js
+++ Tests/null-Mapping-qunit-tests.js
@@ -1,17 +1,21 @@
 ﻿/// <reference path="FromTo-Mapping-qunit-tests.js" />
 var model, updatedModel, modelResult;
-module("Null Tests", {
+module("Null Mapping Tests", {
     setup: function () {
         //ko.viewmodel.logging = true;
 
         model = {
             Prop1: null,
-            Prop2: "test2"
+            Prop2: "test2",
+            Prop3: {},
+            Prop4: null
         };
 
         updatedModel = {
             Prop1: "test2",
-            Prop2: null
+            Prop2: null,
+            Prop3: null,
+            Prop4: {}
         };
 
     },
@@ -23,140 +27,251 @@ module("Null Tests", {
     }
 });
 
-//test("Extend", function () {
+test("Extend String Null", function () {
 
-//    var viewmodel = ko.viewmodel.fromModel(model, {
-//        extend: {
-//            "{root}.Prop1": function (val) {
-//                val.isValid = ko.computed(function () {
-//                    return val() && val().length;
-//                });
-//            },
-//            "{root}.Prop2": function (val) {
-//                val.isValid = ko.computed(function () {
-//                    return val() && val() > 0;
-//                });
-//            }
-//        }
-//    });
+    var viewmodel = ko.viewmodel.fromModel(model, {
+        extend: {
+            "{root}.Prop1": function (val) {
+                val.isValid = ko.computed(function () {
+                    return val() != null && val().length > 0;
+                });
+            },
+            "{root}.Prop2": function (val) {
+                val.isValid = ko.computed(function () {
+                    return val() != null && val().length > 0;
+                });
+            }
+        }
+    });
 
-//    deepEqual(viewmodel.Prop1(), model.Prop1, "Null Prop Test");
-//    deepEqual(viewmodel.Prop2(), model.Prop2, "String Prop Test");
-//    deepEqual(viewmodel.isValid(), true, "Extension check");
+    deepEqual(viewmodel.Prop1(), model.Prop1, "Null Prop Test");
+    deepEqual(viewmodel.Prop1.isValid(), false, "Null Prop Extend Test");
+    deepEqual(viewmodel.Prop2(), model.Prop2, "String Prop Test");
+    deepEqual(viewmodel.Prop2.isValid(), true, "Null Prop Extend Test");
 
-//    ko.viewmodel.updateFromModel(viewmodel, updatedModel);
+    ko.viewmodel.updateFromModel(viewmodel, updatedModel);
 
-//    deepEqual(viewmodel.Prop1(), updatedModel.Prop1, "Null to Value Update Test");
-//    deepEqual(viewmodel.Prop2(), updatedModel.Prop2, "Value to Null Update Test");
-//    deepEqual(viewmodel.isValid(), true, "Extension check");
+    deepEqual(viewmodel.Prop1(), updatedModel.Prop1, "Null Prop Test");
+    deepEqual(viewmodel.Prop1.isValid(), true, "Null Prop Extend Test");
+    deepEqual(viewmodel.Prop2(), updatedModel.Prop2, "String Prop Test");
+    deepEqual(viewmodel.Prop2.isValid(), false, "Null Prop Extend Test");
 
-//    modelResult = ko.viewmodel.toModel(viewmodel);
+    modelResult = ko.viewmodel.toModel(viewmodel);
 
-//    deepEqual(modelResult, updatedModel, "Result Object Comparison");
-//});
+    deepEqual(modelResult, updatedModel, "Result Object Comparison");
+});
 
-//test("Append property", function () {
+test("Extend Object Null", function () {
 
-//    var viewmodel = ko.viewmodel.fromModel(model, {
-//        append: ["{root}.Prop1", "{root}.Prop2"]
-//    });
+    var viewmodel = ko.viewmodel.fromModel(model, {
+        extend: {
+            "{root}.Prop3": function (val) {
+                val.isValid = ko.computed(function () {
+                    return ko.utils.unwrapObservable(val) != null;
+                });
+            }, 
+            "{root}.Prop4": function (val) {
+                val.isValid = ko.computed(function () {
+                    return ko.utils.unwrapObservable(val) != null;
+                });
+            }
+        }
+    });
+
+    deepEqual(typeof viewmodel.Prop3, "object", "Object Prop Test");
+    deepEqual(viewmodel.Prop3.isValid(), true, "Object Prop Extend Test");
+    deepEqual(viewmodel.Prop4(), null, "Null Prop Test");
+    deepEqual(viewmodel.Prop4.isValid(), false, "Null Prop Extend Test");
+
+    ko.viewmodel.updateFromModel(viewmodel, updatedModel);
+
+    deepEqual(viewmodel.Prop3, null, "Object to Null Prop Update Test");
+    deepEqual(viewmodel.Prop4(), updatedModel.Prop4, "Null to Object Update Prop Test");
+    deepEqual(viewmodel.Prop4.isValid(), true, "Null to Object Update Prop Extend Test");
+
+    modelResult = ko.viewmodel.toModel(viewmodel);
+
+    deepEqual(modelResult, updatedModel, "Result Object Comparison");
+});
+
+test("Extend Object Null", function () {
+
+    var viewmodel = ko.viewmodel.fromModel(model, {
+        extend: {
+            "{root}.Prop1": function (val) {
+                if (!ko.isObservable(val)) {
+                    val = ko.observable(val)
+                }
+                return val;
+            },
+            "{root}.Prop2": function (val) {
+                if (!ko.isObservable(val)) {
+                    val = ko.observable(val)
+                }
+                return val;
+            },
+            "{root}.Prop3": function (val) {
+                if (!ko.isObservable(val)) {
+                    val = ko.observable(val)
+                }
+                return val;
+            },
+            "{root}.Prop4": function (val) {
+                if (!ko.isObservable(val)) {
+                    val = ko.observable(val)
+                }
+                return val;
+            }
+        }
+    });
+
+    deepEqual(viewmodel.Prop1(), model.Prop1);
+    deepEqual(viewmodel.Prop2(), model.Prop2);
+    deepEqual(viewmodel.Prop3(), model.Prop3);
+    deepEqual(viewmodel.Prop4(), model.Prop4);
+
+    ko.viewmodel.updateFromModel(viewmodel, updatedModel);
+
+    deepEqual(viewmodel.Prop1(), updatedModel.Prop1);
+    deepEqual(viewmodel.Prop2(), updatedModel.Prop2);
+    deepEqual(viewmodel.Prop3(), updatedModel.Prop3);
+    deepEqual(viewmodel.Prop4(), updatedModel.Prop4);
+
+    modelResult = ko.viewmodel.toModel(viewmodel);
+
+    deepEqual(updatedModel, modelResult);
+});
+
+test("Append property", function () {
+
+    var viewmodel = ko.viewmodel.fromModel(model, {
+        append: ["{root}.Prop1", "{root}.Prop2"]
+    });
 	
-//    deepEqual(viewmodel.Prop1, model.Prop1, "Null to Value Update Test");
-//    deepEqual(viewmodel.Prop2, model.Prop2, "Value to Null Update Test");
+    deepEqual(viewmodel.Prop1, model.Prop1, "Null to Value Update Test");
+    deepEqual(viewmodel.Prop2, model.Prop2, "Value to Null Update Test");
 	
-//	ko.viewmodel.updateFromModel(viewmodel, updatedModel);
+	ko.viewmodel.updateFromModel(viewmodel, updatedModel);
 	
-//	notEqual(viewmodel.Prop1, updatedModel.Prop1, "Null to Value Update Test");
-//	notEqual(viewmodel.Prop2, updatedModel.Prop2, "Value to Null Update Test");
+	notEqual(viewmodel.Prop1, updatedModel.Prop1, "Null to Value Update Test");
+	notEqual(viewmodel.Prop2, updatedModel.Prop2, "Value to Null Update Test");
 	
-//	modelResult = ko.viewmodel.toModel(viewmodel);
+	modelResult = ko.viewmodel.toModel(viewmodel);
 	
-//	notEqual(modelResult.Prop1, updatedModel.Prop1, "Null to Value Update Test");
-//	notEqual(modelResult.Prop2, updatedModel.Prop2, "Value to Null Update Test");
+	notEqual(modelResult.Prop1, updatedModel.Prop1, "Null to Value Update Test");
+	notDeepEqual(modelResult.Prop2, updatedModel.Prop2, "Value to Null Update Test");
 
-//});
+});
 
-//test("Custom basic", function () {
+test("Custom basic", function () {
 
-//    var viewmodel = ko.viewmodel.fromModel(model, {
-//        custom: {
-//			"{root}.date":function (date) {
-//				return ko.observable(date.valueOf());
-//			}
-//		}
-//    });
+    var viewmodel = ko.viewmodel.fromModel(model, {
+        custom: {
+			"{root}.Prop1":function (val) {
+			    return ko.observable(val);
+			},
+			"{root}.Prop2": function (val) {
+			    return ko.observable(val);
+			},
+			"{root}.Prop3": function (val) {
+			    return ko.observable(val);
+			},
+			"{root}.Prop4": function (val) {
+			    return ko.observable(val);
+			}
+		}
+    });
 
-//    deepEqual(viewmodel.stringProp(), model.stringProp, "From Model String Test");
-//    deepEqual(viewmodel.id(), model.id, "From Model Number Test");
-//    deepEqual(viewmodel.date(), model.date.valueOf(), "From Model Date Test");
+    deepEqual(viewmodel.Prop1(), model.Prop1);
+    deepEqual(viewmodel.Prop2(), model.Prop2);
+    deepEqual(viewmodel.Prop3(), model.Prop3);
+    deepEqual(viewmodel.Prop4(), model.Prop4);
 
-//    ko.viewmodel.updateFromModel(viewmodel, updatedModel);
+    ko.viewmodel.updateFromModel(viewmodel, updatedModel);
 
-//	deepEqual(viewmodel.stringProp(), updatedModel.stringProp, "Update String Test");
-//    deepEqual(viewmodel.id(), updatedModel.id, "Update Number Test");
-//    notEqual(viewmodel.date(), updatedModel.date.valueOf(), "Update Date Test");
+    deepEqual(viewmodel.Prop1(), updatedModel.Prop1);
+    deepEqual(viewmodel.Prop2(), updatedModel.Prop2);
+    deepEqual(viewmodel.Prop3(), updatedModel.Prop3);
+    deepEqual(viewmodel.Prop4(), updatedModel.Prop4);
 
-//    modelResult = ko.viewmodel.toModel(viewmodel);
+    modelResult = ko.viewmodel.toModel(viewmodel);
 
-//    deepEqual(modelResult.stringProp, updatedModel.stringProp, "To Model String Test");
-//    deepEqual(modelResult.id, updatedModel.id, "To Model Number Test");
-//    deepEqual(modelResult.date, updatedModel.date, "To Model Date Test");
-//});
+    deepEqual(updatedModel, modelResult);
 
-//test("Custom map and unmap", function () {
+});
 
-//    var viewmodel = ko.viewmodel.fromModel(model, {
-//        custom: {
-//			"{root}.date":{
-//				map: function (date) {
-//					return ko.observable(date.valueOf());
-//				},
-//				unmap: function(date){
-//					return new Date(date());
-//				}
-//			}
-//        }
-//    });
+test("Custom map and unmap", function () {
 
-//    deepEqual(viewmodel.stringProp(), model.stringProp, "From Model String Test");
-//    deepEqual(viewmodel.id(), model.id, "From Model Number Test");
-//    deepEqual(viewmodel.date(), model.date.valueOf(), "From Model Date Test");
+    var viewmodel = ko.viewmodel.fromModel(model, {
+        custom: {
+            "{root}.Prop1": {
+                map: function (val) {
+                    return ko.observable(val);
+                },
+                unmap: function (val) {
+                    return val();
+                }
+            },
+            "{root}.Prop2": {
+                map: function (val) {
+                    return ko.observable(val);
+                },
+                unmap: function (val) {
+                    return val();
+                }
+            },
+            "{root}.Prop3": {
+                map: function (val) {
+                    return ko.observable(val);
+                },
+                unmap: function (val) {
+                    return val();
+                }
+            },
+            "{root}.Prop4": {
+                map: function (val) {
+                    return ko.observable(val);
+                },
+                unmap: function (val) {
+                    return val();
+                }
+            }
+        }
+    });
 
-//    ko.viewmodel.updateFromModel(viewmodel, updatedModel);
+    deepEqual(viewmodel.Prop1(), model.Prop1);
+    deepEqual(viewmodel.Prop2(), model.Prop2);
+    deepEqual(viewmodel.Prop3(), model.Prop3);
+    deepEqual(viewmodel.Prop4(), model.Prop4);
 
-//	deepEqual(viewmodel.stringProp(), updatedModel.stringProp, "Update String Test");
-//    deepEqual(viewmodel.id(), updatedModel.id, "Update Number Test");
-//    deepEqual(viewmodel.date(), updatedModel.date.valueOf(), "Update Date Test");
+    ko.viewmodel.updateFromModel(viewmodel, updatedModel);
 
-//    modelResult = ko.viewmodel.toModel(viewmodel);
+    deepEqual(viewmodel.Prop1(), updatedModel.Prop1);
+    deepEqual(viewmodel.Prop2(), updatedModel.Prop2);
+    deepEqual(viewmodel.Prop3(), updatedModel.Prop3);
+    deepEqual(viewmodel.Prop4(), updatedModel.Prop4);
 
-//    deepEqual(modelResult.stringProp, updatedModel.stringProp, "To Model String Test");
-//    deepEqual(modelResult.id, updatedModel.id, "To Model Number Test");
-//    deepEqual(modelResult.date, updatedModel.date, "To Model Date Test");
-//});
+    modelResult = ko.viewmodel.toModel(viewmodel);
 
-//test("Exclude", function () {
+    deepEqual(updatedModel, modelResult);
+});
 
-//    var viewmodel = ko.viewmodel.fromModel(model, {
-//        exclude: ["{root}.stringProp"]
-//    });
+test("Exclude", function () {
 
-//	equal(viewmodel.hasOwnProperty("stringProp"), false, "From Model String Prop Not Exist");
-//    deepEqual(viewmodel.id(), model.id, "From Model Number Test");
-//    deepEqual(viewmodel.date(), model.date, "From Model Date Test");
+    var viewmodel = ko.viewmodel.fromModel(model, {
+        exclude: ["{root}.Prop2"]
+    });
 
-//    ko.viewmodel.updateFromModel(viewmodel, updatedModel);
+    equal(viewmodel.hasOwnProperty("Prop2"), false, "From Model String Prop Not Exist");
 
-//	equal(viewmodel.hasOwnProperty("stringProp"), false, "Update... String Prop Not Exist");
-//    deepEqual(viewmodel.id(), updatedModel.id, "Update Number Test");
-//    deepEqual(viewmodel.date(), updatedModel.date, "Update Date Test");
+    ko.viewmodel.updateFromModel(viewmodel, updatedModel);
 
-//    modelResult = ko.viewmodel.toModel(viewmodel);
+    equal(viewmodel.hasOwnProperty("Prop2"), false, "Update... String Prop Not Exist");
 
-//    notEqual(modelResult.stringProp, updatedModel.stringProp, "To Model String Test");
-//    deepEqual(modelResult.id, updatedModel.id, "To Model Number Test");
-//    deepEqual(modelResult.date, updatedModel.date, "To Model Date Test");
-//});
+    modelResult = ko.viewmodel.toModel(viewmodel);
+
+    equal(modelResult.hasOwnProperty("Prop2"), false, "Update... String Prop Not Exist");
+});
 
 
 
