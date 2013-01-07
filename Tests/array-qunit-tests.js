056ddb036a94cfdab1f0aa13421c5dd056639ef8
Dave Herren <David.Herren@yahoo.com>
2013-01-06 22:55:50 -0500

    Merge from main of v1.1.0

---
+++ Tests/array-qunit-tests.js
@@ -0,0 +1,39 @@
+﻿
+var model;
+module("toModel Basic", {
+    setup: function () {
+        //ko.viewmodel.logging = true;
+        model = {
+            array: []
+        };
+    },
+    teardown: function () {
+        //ko.viewmodel.logging = false;
+        model = undefined;
+    }
+});
+
+
+test("array push pop", function () {
+    var viewmodel, result;
+
+    viewmodel = ko.viewmodel.fromModel(model);
+    viewmodel.array.push({ test: true });
+    result = viewmodel.array.pop();
+
+    assert(result.test(), true);
+
+});
+
+test("array push pop", function () {
+    var viewmodel, result;
+
+    viewmodel = ko.viewmodel.fromModel(model);
+    viewmodel.array.push({ test: true }, true);
+    result = viewmodel.array.pop();
+
+    assert(result.test, true);
+
+});
+
+
