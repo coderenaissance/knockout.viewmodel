45bf094b724fe297b5827c1f2da82b81927e6515
Dave Herren <David.Herren@yahoo.com>
2013-01-16 22:10:38 -0500

    Added failing unit test for issue #18

--- Tests/issues-qunit-tests.js
+++ Tests/issues-qunit-tests.js
@@ -1,6 +1,6 @@
 ﻿/// <reference path="FromTo-Mapping-qunit-tests.js" />
 var model, viewmodel, updatedModel, modelResult;
-module("Basic Null Tests", {
+module("Issue Tests", {
     setup: function () {
         //ko.viewmodel.logging = true;
 
@@ -14,24 +14,13 @@ module("Basic Null Tests", {
     }
 });
 
-test("Issue 17 - toModel call fails to correctly unwrap fromModel with nested observableArray of strings", function () {
-
-    model = { items: [["a", "b", "c", "d"]] };
-
-    viewmodel = ko.viewmodel.fromModel(model);
-
-    modelResult = ko.viewmodel.toModel(viewmodel);
-
-    deepEqual(model, modelResult);
-
-});
 
 test("Issue 19 - empty array throws an exception on update", function () {
 
     model = { items: [] };
 
     updatedModel = {
-        items:[{
+        items: [{
             id: 5,
             text: "test"
         }]
@@ -42,5 +31,49 @@ test("Issue 19 - empty array throws an exception on update", function () {
     ko.viewmodel.updateFromModel(viewmodel, updatedModel);
 
     deepEqual(viewmodel.items().length, 1);
+
+});
+
+test("Issue 18 - toModel call fails to completely strip extended functions and internal properties", function () {
+
+    model = {
+        items: [{
+            id: 5,
+            text: "test"
+        }],
+        obj: {
+            text: "test"
+        }
+    };
+
+    viewmodel = ko.viewmodel.fromModel(model, {
+        id: ["{root}.items[i].id"],
+        extend: {
+            "{root}.obj": function (obj) {
+                obj.getTextLength = function () {
+                    return obj.text().length;
+                }
+            }
+        }
+    });
+
+    modelResult = ko.viewmodel.toModel(viewmodel);
+
+    deepEqual(modelResult.items[0].hasOwnProperty("..idName"), false);
+    deepEqual(typeof modelResult.obj.getTextLength, "undefined");
+    deepEqual(modelResult.obj.hasOwnProperty("getTextLength"), false);
+
+});
+
+
+test("Issue 17 - toModel call fails to correctly unwrap fromModel with nested observableArray of strings", function () {
+
+    model = { items: [["a", "b", "c", "d"]] };
+
+    viewmodel = ko.viewmodel.fromModel(model);
+
+    modelResult = ko.viewmodel.toModel(viewmodel);
+
+    deepEqual(model, modelResult);
 
 });
