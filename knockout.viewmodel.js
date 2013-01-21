9ae9a39152660f40319aa67bc30ad182d23bf510
Dave Herren <David.Herren@yahoo.com>
2013-01-20 23:40:38 -0500

    updated comment to reflect 2.0.0

--- knockout.viewmodel.js
+++ knockout.viewmodel.js
@@ -1,4 +1,4 @@
-﻿/*ko.viewmodel.js - version 1.1.5
+﻿/*ko.viewmodel.js - version 2.0.0
 * Copyright 2013, Dave Herren http://coderenaissance.github.com/knockout.viewmodel/
 * License: MIT (http://www.opensource.org/licenses/mit-license.php)*/
 /*jshint eqnull:true, boss:true, loopfunc:true, evil:true, laxbreak:true, undef:true, unused:true, browser:true, immed:true, devel:true, sub: true, maxerr:50 */
